cddb069450d58dd498ee11e6d24bdfed233f126a
Javad Choopani <86573037+javadch2021@users.noreply.github.com>
2021-08-14 14:41:46 +0430

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const fs = require('fs');
-
+const fs = require("fs");
+const path = require("path");
 
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    `<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>`
-  );
+  res.sendFile(path.join(__dirname,'../','views','add-product.html'));
 });
 
 router.post("/add-product", (req, res, next) => {
-  fs.writeFileSync('product.txt',req.body.title)
+  fs.writeFileSync("product.txt", req.body.title);
   res.redirect("/");
 });
 
 module.exports = router;
+
