0e72f9051b5d3cedd2c6f81ab90093fa9e140d5b
Javad Choopani <86573037+javadch2021@users.noreply.github.com>
2021-08-14 14:41:06 +0430

    Update NodeJs.js

--- NodeJs.js
+++ NodeJs.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
-
+const path = require("path");
 
 const app = express();
 
@@ -9,12 +8,14 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname,'public')))
 
-app.use('/admin',adminRoutes);
+app.use("/admin", adminRoutes);
 
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send(`<h1>Page Not Found!</h1>`);
+  res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
 app.listen(3000);
+
